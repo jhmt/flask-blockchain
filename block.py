2ae08fd128eb4c2e53965895d2343249678c5044
Jun Hashimoto <jun.hashimoto@trademe.co.nz>
2019-11-02 21:17:11 +1300

    updating block class

--- block.py
+++ block.py
@@ -1,3 +1,5 @@
+import json
+import hashlib
 
 class Block:
     def __init__(self, timestamp, nonce, previousHash, transactions):
@@ -5,3 +7,10 @@ class Block:
         self.nonce = nonce
         self.previousHash = previousHash
         self.transactions = transactions
+    
+    def to_json(self):
+        return json.dumps(self.__dict__, indent=4, sort_keys=True, default=str)
+
+    def hash(self):
+        return hashlib.sha256(self.to_json().encode()).hexdigest()
+
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
- 
+from block import Block
+from datetime import datetime
+
 if __name__ == "__main__":
+    b = Block(datetime.now(), 0, '', [])
+    b.hash()
